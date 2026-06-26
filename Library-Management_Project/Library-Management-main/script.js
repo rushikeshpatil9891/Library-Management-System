6191edd04be1f2b01a19204d8ff691a2e1af52c7
Rushikesh Patil <166911774+rushikeshpatil9891@users.noreply.github.com>
2026-06-26 11:19:11 +0530

    Update script.js
    Go Ahead....

--- Library-Management_Project/Library-Management-main/script.js
+++ Library-Management_Project/Library-Management-main/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.AddEventListner('DOMContentLoaded', (event) => {
     document.getElementById('search-button').addEventListener('click', searchBook);
 });
 
